709656d80b7a8d6b9d359ad50a8c73cdbae3967a
samuel <shr1287@gmail.com>
2016-11-24 20:36:11 -0600

    primer mensaje

---
+++ user2.py
@@ -0,0 +1,13 @@
+from usuario import Usuario
+
+user = Usuario()
+
+print(type(user))
+
+print("Nombre de usuario")
+print(user.nombre)
+
+respuesta = user.obtenInfo()
+
+print("Respuestaaaa")
+print(type(respuesta))
---
+++ usuario.py
@@ -0,0 +1,13 @@
+class  Usuario:
+	nombre = ""
+	"""docstring for  user"""
+	def __init__(self):
+  		self.url = "https://graph.facebook.com/v2.8/me"
+  		self.token = "EAACEdEose0cBAJ9ZBZCOi9QKOYSFiNc7r3RORKIdI30iADohJZA2ismjcCdMmI2zZCmiD2c307qXiOyxfTpXpZBgbkv0g2ii6ObKpUSL495U8iGzsWdbeAaFVC8tTden7RAE6NVTnqRgv7zrgX7cxMpxJensHXMVGiWIYdglCswZDZD"
+
+  	def obtenInformacion(self):
+  		parametros = {"acces_token": self.token}
+  		resultado = request.get(self.url, params=parametros).json
+  		print (resultado)
+  		self.nombre = resultado["name"]
+  		return resultado
